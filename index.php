234c73207aedfe1818ca42f4057790693251ed15
Josef Zacek <info@josefzacek.com>
2014-06-23 13:56:06 +0100

    added $contact_form to headers

--- index.php
+++ index.php
@@ -28,8 +28,8 @@ if(isset($_POST['contact-form-submit']))
 		
 			$headers  = 'MIME-Version: 1.0' . "\r\n";
 		    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-		    //$headers .= 'To:' . $email . "\r\n";	
-		    $headers .= 'From: ' . $email . "\r\n" .
+		 	
+		    $headers .= 'From: ' . $contact_email . "\r\n" .
 						'X-Mailer: PHP/' . phpversion();
             
                 // check if variables not empty
